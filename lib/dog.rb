0a32a9d5b8f1c01b30240f9f80868a8b1f9d10f4
Brandon Dusch <brandondusch@gmail.com>
2020-03-08 13:23:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Add your code here
+class Dog  
+  @@all = [] 
+  
+  attr_accessor :name, :breed 
+  
+  def initialize(name, breed) 
+    @name = name 
+    @breed = breed 
+    @@all << self 
+  end
+  
+  def self.all
+    @all 
+  end
+  
+  def self.print_all 
+    @@all.each {|name|
+      puts "#{name}"
+    }
+  end
+end
